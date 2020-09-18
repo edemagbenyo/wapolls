22b2ae5cc5d70009c199e416951be4dcca8c25ed
edemagbenyo <edem.agbenyo@gmail.com>
2020-09-18 07:01:12 +0000

    trim days hours and minutes. create report div

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,11 +1,17 @@
+const form = document.querySelector('form');
 const add_choice_btn = document.querySelector(".choice_btn");
 const choices_inputs_div = document.querySelector(".choices");
 const choices_inputs = document.querySelectorAll(".choice");
 const create_poll_btn = document.querySelector(".create");
-const name = document.querySelector("#name");
+const name_input = document.querySelector("#name");
 const question_input = document.querySelector("#question");
+const days_input = document.querySelector("#days");
+const hours_input = document.querySelector("#hours");
+const minutes_input = document.querySelector("#minutes");
+const report_div = document.querySelector('.report')
 
 
+//handle creation and deletion of choice input
 let choice_count = 2;
 add_choice_btn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -33,22 +39,54 @@ add_choice_btn.addEventListener("click", (e) => {
   }
 });
 
+
 question_input.addEventListener("input",(e)=>{
   checkSavable()
 })
 
 choices_inputs_div.addEventListener("input",(e)=>{
-  console.log("inputs triggered")
   checkSavable()
 })
 create_poll_btn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("save poll")
+  const choices_inputs = document.querySelectorAll(".choice");
+
+  //name
+  const name_val = name_input.value;
+  const question_val = question_input.value;
+  let choices = [];
+  for(let ch of Array.from(choices_inputs)){
+    choices.push(ch.value)
+  }
+  const days_val = days_input.value;
+  const hours_val = hours_input.value;
+  const minutes_val = minutes_input.value;
+  
+  const form_data ={
+    name_val,
+    question_val,
+    choices,
+    days_val,
+    hours_val,
+    minutes_val,
+  }
+  form.reset()
+  generateReport(form_data)
+
 });
 
+function generateReport(data){
+  console.log(data)
+  report_div.style.visibility="visible"
+  const q = document.createElement('h2')
+  q.style.color="#FFFFFF"
+  q.style.fontSize="1.3em"
+  q.textContent=data.question_val
+  report_div.append(q)
+  form.style.display="none"
+}
 
 function checkSavable(){
-  console.log(choices_inputs[0].value,choices_inputs[0])
   if(question_input.value!=="" && choices_inputs[0].value!=="" && choices_inputs[1].value!=="" ){
     create_poll_btn.disabled=false
   }else if(question_input.value=="" || choices_inputs[0].value=="" || choices_inputs[1].value=="" ){
